a9dc7480d58fe51e6c13af596c572409fa601c62
BaastPriest <Aul0na@rambler.ru>
2017-01-24 00:17:27 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,26 +4,25 @@ public class MyFirstProgram
 {
 	public static void main(String[] args) 
 	{
-      hello("world");
+      Point p1 = new Point(0.0, 0.0);
+      Point p2 = new Point(-1.0, -1.0);
 
-      double l = 5;
-      System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+      double dist = distance(p1, p2);
 
-      double a = 4;
-      double b = 6;
-      System.out.println("Площадь треугольника со сторонами " + a + " и " + b + " = " + area (a, b) );
+      System.out.println(dist);
+
+      hello ("World");
+
+      Rectangle r = new Rectangle(4,6);
+      System.out.println("Площадь треугольника со сторонами " + r.a + " и " + r.b + " = " + r.area () );
   }
     public static void hello(String somebody) {
         System.out.println("Hello," + somebody + "!");
     }
 
-    public static double area(double len){
-        return len * len;
+    public static double distance(Point p1, Point p2){
+
+        return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)));
+
     }
-
-    public static double area(double a, double b ) {
-        return a * b;
-    }
-
-
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 23.01.2017.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double a, double b) {
+    this.x = a;
+    this.y = b;
+
+  }
+
+}
