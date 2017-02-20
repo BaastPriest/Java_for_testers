171d021941fe3d6d7d38ea2f549013b0f64a7fba
BaastPriest <Aul0na@rambler.ru>
2017-02-20 14:21:39 +0300

    3.9 Проверять перед каждым переходом, действительно ли этот переход нужно делать
    Остался момент с тем, что у меня не было ранее метода gotoHomePage. Я
    его добавила, но он нигде не используется.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -11,6 +11,18 @@ public class NavigationHelper extends HeplerBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1")) //логическая операция И
+             && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+             && isElementPresent(By.name("new")) ) {
+      return;
+    }
     click(By.linkText("groups"));
+  }
+
+  public void gotoHomePage() {
+           if (isElementPresent(By.id("maintable"))) {
+             return;
+           }
+           click(By.linkText("home"));
   }
 }
