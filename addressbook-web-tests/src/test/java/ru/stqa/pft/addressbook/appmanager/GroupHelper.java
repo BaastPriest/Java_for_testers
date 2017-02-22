865695d04bc7c1cb24fab22c718a4e89fdcd9470
BaastPriest <Aul0na@rambler.ru>
2017-02-22 22:57:29 +0300

    Исправила ошибку. тесты работают
    Проблема была в  том, что в коде находися нужный элемент, но в следующей
    строчке происходил клик всех элементов(чекбоксов), а выбранный элемент
    после повторного клика не выбирался.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -40,7 +40,6 @@ public class GroupHelper extends HeplerBase {
 
     public void selectGroup(int index) {
         wd.findElements(By.name("selected[]")).get(index).click();
-        click(By.name("selected[]"));
     }
 
     public void initGroupModification() {
