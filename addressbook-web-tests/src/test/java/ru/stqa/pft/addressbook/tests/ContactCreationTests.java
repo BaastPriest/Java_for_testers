cf494f3d3f449c28294fc79c818c4854294fcd1b
BaastPriest <Aul0na@rambler.ru>
2017-03-25 00:18:09 +0300

    6.1 проверила, что работает.  Пути к файлам и директориям Гиперссылка

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test //(enabled = false)
+    @Test (enabled = false)
     public void testContactCreation() {
       app.goTo().homePage();
       Contacts before = app.contact().all();
@@ -26,7 +26,7 @@ public class ContactCreationTests extends TestBase {
               before.withAdded(contact.withId(after.stream().mapToInt((c)->c.getId()).max().getAsInt()))));
     }
 
-    @Test (enabled = false)
+    @Test //(enabled = false)
     public void testBadContactCreation()  {
         app.goTo().homePage();
         Contacts before = app.contact().all();
@@ -39,12 +39,12 @@ public class ContactCreationTests extends TestBase {
         assertThat(after, equalTo(before));
     }
 
-    @Test (enabled = false)
+    @Test //(enabled = false)
     public void testCurrentDir(){
         File currentDir = new File("");
         System.out.println(currentDir.getAbsolutePath());
         File photo = new File("src/test/resources/cat.jpg");
         System.out.println(currentDir.getAbsolutePath());
-        System.out.println(photo.exists());
+        System.out.println(photo.exists());// тут можно проверить права доступа,создать его,удалить и т.д.
     }
   }
